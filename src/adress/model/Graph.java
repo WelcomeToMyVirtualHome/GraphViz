581a5a0687ab889f672834cc3c84c4bc0a12a6dd
Jakub Sobolewski <37193264+WelcomeToMyVirtualHome@users.noreply.github.com>
2018-07-10 22:47:39 +0200

    removed menu bar and unused imports

--- src/adress/model/Graph.java
+++ src/adress/model/Graph.java
@@ -2,7 +2,6 @@ package adress.model;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.SplittableRandom;
 
 public class Graph {
--- src/adress/view/RootLayoutController.java
+++ src/adress/view/RootLayoutController.java
@@ -1,24 +1,12 @@
 package adress.view;
 
-import adress.model.GraphVisual;
 import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Screen;
 
 public class RootLayoutController {
-	
-	@FXML
-	private Slider c1;
-	@FXML
-	private Slider c2;
-	@FXML
-	private Slider c3;
-	@FXML
-	private Slider c4;
-	@FXML
-	private Slider c5;
+
 	@FXML
 	private BorderPane pane;
 	
@@ -30,30 +18,6 @@ public class RootLayoutController {
         pane.setPrefWidth(size);
         pane.setMaxHeight(size);
         pane.setMaxWidth(size);
-		initSliders();
-	}
-
-	public void initSliders() {
-		c1.setValue(0.005);
-		c2.setValue(0.002);
-		c3.setValue(35);
-		c4.setValue(0.005);
-		c5.setValue(100);
-		c1.valueProperty().addListener((observable, oldValue, newValue) -> {
-			GraphVisual.setC1((double) newValue);
-		});
-		c2.valueProperty().addListener((observable, oldValue, newValue) -> {
-			GraphVisual.setC2((double) newValue);
-		});
-		c3.valueProperty().addListener((observable, oldValue, newValue) -> {
-			GraphVisual.setC3((double) newValue);
-		});
-		c4.valueProperty().addListener((observable, oldValue, newValue) -> {
-			GraphVisual.setC4((double) newValue);
-		});
-		c5.valueProperty().addListener((observable, oldValue, newValue) -> {
-			GraphVisual.setC5(newValue.intValue());
-		});
 	}
 
 }
